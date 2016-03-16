a0414a6b81df15c8db8193ef81e33816ac7cfb74
JosephPena <arcx354@gmail.com>
2016-03-16 18:56:48 -0500

    added geolocation and timestamps for riders summoning drivers

--- client/components/MapView.js
+++ client/components/MapView.js
@@ -1,36 +1,26 @@
 import React from 'react';
 import { GoogleMapLoader, GoogleMap } from 'react-google-maps';
 
-export function MapView(props) {
-
-  function errorError() {
-    alert("Location can't be found");
-  }
+var location = {}
+var options = {timeout:6000};
 
   if (navigator.geolocation) {
-    navigator.geolocation.getCurrentPosition(function (data) {
-    console.log('all the location data', data);
-  });
-
-    navigator.geolocation.getCurrentPosition(function (data) {
-    coord.lat = data.coords.latitude;
-    lng = data.coords.longitude;
-  });
+    navigator.geolocation.watchPosition(function (data, err, options) {
+    location.lat = data.coords.latitude;
+    location.lng = data.coords.longitude;
+	console.log('Rider Time Summoned:', new Date(data.timestamp))
+})}
 
     //clearWatch()    watchPosition()    getCurrentPosition()
-    navigator.geolocation.getCurrentPosition(GoogleMap, errorError);
-  };
+    // navigator.geolocation.getCurrentPosition(GoogleMap, errorError);
 
-  var austin = {
-	  lat: 30.2672,
-	  lng: -97.7431,
-	};
+export function MapView(props) {
   return (
     <div className='map'>
       <GoogleMapLoader
       ref={(map) => console.log(map)}
       containerElement={ <div style={{ height: '70%' }} /> }
-      googleMapElement={ <GoogleMap defaultZoom={14} defaultCenter={{ lat: props.lat, lng: this.props.lng }}>
+      googleMapElement={ <GoogleMap defaultZoom={14} defaultCenter={{ lat: location.lat, lng: location.lng }}>
       </GoogleMap> }
       />
     </div>
