e4f75ee876c7ce9898441515815d3941677b6f68
JosephPena <arcx354@gmail.com>
2016-03-16 15:55:59 -0500

    google maps edits

--- client/components/MapView.js
+++ client/components/MapView.js
@@ -1,45 +1,113 @@
 import React from 'react';
-import { GoogleMapLoader, GoogleMap } from 'react-google-maps';
+import { GoogleMapLoader, GoogleMap , Marker, DirectionsRenderer } from 'react-google-maps';
 
-export function MapView(props) {
+// var location = {}
+// var options = {timeout:6000};
 
-  function errorError() {
-    alert("Location can't be found");
-  }
+// var trackPosition = navigator.geolocation.watchPosition(function(current) {
+//   console.log( "Newer Position Found" );
 
-  if (navigator.geolocation) {
+//          Set the new position of the existing marker.
+//          updateMarker(
+//            locationMarker,
+//            position.coords.latitude,
+//            position.coords.longitude,
+//            "Updated / Accurate Position"
+//          );
+
+//        }
+//      );
+
+// function positionWatcher(input){
+//   if (navigator.geolocation) {
+//     navigator.geolocation.watchPosition(function (data, err, options) {
+//     location.lat = data.coords.latitude;
+//     location.lng = data.coords.longitude;
+//  console.log('Rider Time Summoned:', new Date(data.timestamp))
+// })}
+// }
 
     //clearWatch()    watchPosition()    getCurrentPosition()
-    navigator.geolocation.getCurrentPosition(GoogleMap, errorError);
-  } else {
-    alert('Your browser does not support Geolocation.');
-  };
+    // navigator.geolocation.getCurrentPosition(GoogleMap, errorError);
 
-  var austin = {
-    lat: 30.2672,
-    lng: -97.7431,
-  };
+export function MapView ({ match, location, onDirectionsResult, directions }) {
 
-  return (
-<<<<<<< b7053ad7893432082735b56a39910a5040fb9619
-    <div className='map'>
+const DirectionsService = new google.maps.DirectionsService();
+    DirectionsService.route({
+    origin: {lat:41.8507300, lng:-87.6512600},
+    destination: {lat:41.8525800, lng:-87.6514100},
+      travelMode: google.maps.TravelMode.DRIVING,
+    }, (result, status) => {
+      onDirectionsResult(result);
+      console.log('these are results', result)
+      console.log('these are status', status)
+      }
+    )
+
+
+ return (
+   <div className='map'>
       <GoogleMapLoader
       containerElement={ <div style={{ height: '70%' }} /> }
-      googleMapElement={
-        <GoogleMap
-          defaultZoom={14}
-          defaultCenter={{ lat: austin.lat, lng: austin.lng }}
-        />
-      }
+      googleMapElement={ <GoogleMap defaultZoom={14} defaultCenter={ location } >
+      {directions ? <DirectionsRenderer directions={directions} /> : null}
+      </GoogleMap> }
       />
     </div>
-	);
-=======
-  <div className='map'>
-    <GoogleMapLoader
-      containerElement={ <div style={{ height: '90%' }} /> }
-      googleMapElement={ <GoogleMap defaultZoom={14} defaultCenter={{ lat: 30.2672, lng: -97.7431 }}></GoogleMap> }/>
-  </div>
   );
->>>>>>> (feat) more maps updates
 };
+
+
+
+
+
+
+
+
+function Map ({ match, location }) {
+  return (
+    <div className='map'>
+      match ? 
+        <GoogleMap getDirectionsBetween={riderLocation, driverLocation} />
+      :
+      <GoogleMapLoader
+      ref={(map) => console.log('map data1', map)}
+      containerElement={ <div style={{ height: '70%' }} /> }
+      googleMapElement={ <GoogleMap defaultZoom={14} defaultCenter={ location } >
+      </GoogleMap> }
+      />
+    </div>
+  );
+};
+
+
+
+const mapDispatchToProps = function(dispatch){
+  return (
+    const DirectionsService = new google.maps.DirectionsService();
+    DirectionsService.route({
+    origin: {lat:41.8507300, lng:-87.6512600},
+    destination: {lat:41.8525800, lng:-87.6514100},
+      travelMode: google.maps.TravelMode.DRIVING,
+    }, (result, status) => {
+      onDirectionsResult(result);
+      console.log('these are results', result)
+      console.log('these are status', status)
+      }
+    )
+    )
+}
+
+export const MapView = connect(
+
+
+)
+
+
+
+
+
+
+
+
+
