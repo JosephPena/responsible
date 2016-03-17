2a8f94d29f0366f2a33a2bdb4886a66075da7611
JosephPena <arcx354@gmail.com>
2016-03-17 10:44:48 -0500

    working on active updating geolocation

--- client/components/MapView.js
+++ client/components/MapView.js
@@ -1,8 +1,23 @@
 import React from 'react';
-import { GoogleMapLoader, GoogleMap } from 'react-google-maps';
+import { GoogleMapLoader, GoogleMap , Marker} from 'react-google-maps';
 
 var location = {}
 var options = {timeout:6000};
+
+var trackPosition = navigator.geolocation.watchPosition(function(current) {
+  console.log( "Newer Position Found" );
+
+					// Set the new position of the existing marker.
+					// updateMarker(
+					// 	locationMarker,
+					// 	position.coords.latitude,
+					// 	position.coords.longitude,
+					// 	"Updated / Accurate Position"
+					// );
+
+				}
+			);
+
 
   if (navigator.geolocation) {
     navigator.geolocation.watchPosition(function (data, err, options) {
@@ -18,11 +33,17 @@ export function MapView(props) {
   return (
     <div className='map'>
       <GoogleMapLoader
-      ref={(map) => console.log(map)}
+      ref={(map) => console.log('map data1', map)}
       containerElement={ <div style={{ height: '70%' }} /> }
-      googleMapElement={ <GoogleMap defaultZoom={14} defaultCenter={{ lat: location.lat, lng: location.lng }}>
+      googleMapElement={ <GoogleMap 
+      	ref={(map) => console.log('map data2', map)}
+      	defaultZoom={14} defaultCenter={{ lat: location.lat, lng: location.lng }} onClick={props.onMapClick}>
+        return (
+        <Marker onRightclick={() => props.onMarkerRightclick(index)} />
+        );
       </GoogleMap> }
       />
     </div>
 	);
 };
+
