2338f50d95380482949c98f2fb3e0744d86afeea
JosephPena <arcx354@gmail.com>
2016-03-19 10:57:54 -0500

    google map view updates

--- client/components/MapView.js
+++ client/components/MapView.js
@@ -1,7 +1,36 @@
 import React from 'react';
+<<<<<<< 9f6b2b8978ab6dda289bf616f152507db2c320e4
 import { GoogleMapLoader, GoogleMap , Marker, DirectionsRenderer } from 'react-google-maps';
 import { connect } from 'react-redux'
 
+=======
+import { GoogleMapLoader, GoogleMap , Marker} from 'react-google-maps';
+
+var location = {}
+var options = {timeout:6000};
+
+var trackPosition = navigator.geolocation.watchPosition(function(current) {
+  console.log( "Newer Position Found" );
+
+					// Set the new position of the existing marker.
+					// updateMarker(
+					// 	locationMarker,
+					// 	position.coords.latitude,
+					// 	position.coords.longitude,
+					// 	"Updated / Accurate Position"
+					// );
+
+				}
+			);
+
+
+  if (navigator.geolocation) {
+    navigator.geolocation.watchPosition(function (data, err, options) {
+    location.lat = data.coords.latitude;
+    location.lng = data.coords.longitude;
+	console.log('Rider Time Summoned:', new Date(data.timestamp))
+})}
+>>>>>>> working on active updating geolocation
 
     //clearWatch()    watchPosition()    getCurrentPosition()
     // navigator.geolocation.getCurrentPosition(GoogleMap, errorError);
@@ -44,8 +73,17 @@ function Map ({ match, location, onMapView, directions }) {
       <GoogleMapLoader
       ref={(map) => console.log('map data1', map)}
       containerElement={ <div style={{ height: '70%' }} /> }
+<<<<<<< 9f6b2b8978ab6dda289bf616f152507db2c320e4
       googleMapElement={ <GoogleMap defaultZoom={14} defaultCenter={ location } >
       {directions ? <DirectionsRenderer directions={directions} /> : null}
+=======
+      googleMapElement={ <GoogleMap 
+      	ref={(map) => console.log('map data2', map)}
+      	defaultZoom={14} defaultCenter={{ lat: location.lat, lng: location.lng }} onClick={props.onMapClick}>
+        return (
+        <Marker onRightclick={() => props.onMarkerRightclick(index)} />
+        );
+>>>>>>> working on active updating geolocation
       </GoogleMap> }
       />
     </div>
@@ -84,6 +122,7 @@ const mapDispatchToProps = function (dispatch) {
   };
 };
 
+<<<<<<< 9f6b2b8978ab6dda289bf616f152507db2c320e4
 // const DirectionsService = new google.maps.DirectionsService();
 //     DirectionsService.route(
 //     {
@@ -155,3 +194,5 @@ export const MapView = connect(
 
 
 
+=======
+>>>>>>> working on active updating geolocation
