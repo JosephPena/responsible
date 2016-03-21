b82642b9ae31c8c16c9e236efe207c2a34ac957e
JosephPena <arcx354@gmail.com>
2016-03-21 17:13:52 -0500

    (feat) incorporated markers and animation

--- client/components/MapView.js
+++ client/components/MapView.js
@@ -5,6 +5,16 @@ import { connect } from 'react-redux';
 // overlayMapTypes
 // MapTypes
 function Map({ match, location, directions }) {
+  const state = {
+   markers: [{
+     position: {
+       lat: 30.2705365,
+       lng: -97.7362387,
+     },
+     defaultAnimation: 1,
+   },
+   ],
+ };
   console.log('these are directions', directions);
   return match ?
   (
@@ -23,14 +33,22 @@ function Map({ match, location, directions }) {
   :
   (
     <div className='map'>
-      <GoogleMapLoader
-      ref={(map) => console.log('NO MATCH', map)}
-      containerElement={ <div style={{ height: '70%' }} /> }
-      googleMapElement={ <GoogleMap defaultZoom={14} defaultCenter={ location } >
-      </GoogleMap> }
-      />
+    <GoogleMapLoader
+        ref={(map) => console.log('OHHHH... YOU WALKIN!', map)}
+       containerElement={<div style={{ height: '100%' }} />}
+       googleMapElement={
+        <GoogleMap defaultZoom={16} defaultCenter={ location } >
+         {state.markers.map((marker) => {
+           return (
+        <Marker {...marker} />
+           );
+         })};
+         </GoogleMap>
+       }
+     />
     </div>
   );
 };
 
 export const MapView = connect()(Map);
+
